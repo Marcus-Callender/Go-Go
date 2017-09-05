90b361ddea076e12263fb7d1454035e417e13361
Marcus Callender <Marcus-Callender@users.noreply.github.com>
2017-09-05 19:56:05 +0100

    Finished the basic framework

--- Go-Go/Assets/Code/Players.cs
+++ Go-Go/Assets/Code/Players.cs
@@ -4,14 +4,50 @@ using UnityEngine;
 
 public class Players : MonoBehaviour
 {
+    public bool m_player2 = false;
+
+    public Camera m_camera;
 
     void Start()
     {
-
+        m_camera = GetComponent<Camera>();
     }
 
     void Update()
     {
+        Vector3 _mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+        Debug.DrawRay(_mousePos, Vector3.forward * 15.0f, Color.green);
+        
+        //int mouseDown = Input.GetMouseButtonDown(0) ? (Input.GetMouseButtonDown(1) ? -1 : 1) : 0;
+        int mouseDown = Input.GetMouseButtonDown(0) ? 0 : (Input.GetMouseButtonDown(1) ? 1 : -1);
 
+        Debug.Log("Mouse: " + mouseDown);
+
+        if (mouseDown > -1)
+        {
+            Vector3 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+
+            RaycastHit[] hit = Physics.RaycastAll(mousePos, Vector3.forward * 15.0f);
+
+            foreach (RaycastHit oneHit in hit)
+            {
+                Stone data = oneHit.transform.gameObject.GetComponent<Stone>();
+
+                if (data)
+                {
+                    if (data.ClickStone(mouseDown, m_player2 ? 1 : 0))
+                    {
+                        Debug.Log("Cicked");
+                        m_player2 = !m_player2;
+                    }
+                    else
+                    {
+                        Debug.Log("Click Failed");
+                    }
+                }
+
+                break;
+            }
+        }
     }
 }
--- Go-Go/Assets/Code/Stone.cs
+++ Go-Go/Assets/Code/Stone.cs
@@ -10,12 +10,43 @@ public class Stone : MonoBehaviour
     {
         m_mat = GetComponent<Renderer>();
         
-        m_mat.material.color = Color.yellow;
-        m_mat.material.color = new Color(1.0f, 0.92f, 0.016f, 1.0f);
     }
     
     void Update()
     {
+        
+    }
 
+    public bool ClickStone(int mouseButton, int player)
+    {
+        Debug.Log("Ply: " + player);
+
+        if (mouseButton == 0)
+        {
+            if (player == 0)
+            {
+                m_mat.material.color = new Color(1.0f, 0.92f, 0.016f, 1.0f);
+
+                Debug.Log("Yellow");
+
+                return true;
+            }
+            else if (player == 1)
+            {
+                m_mat.material.color = new Color(1.0f, 0.0f, 1.0f, 1.0f);
+
+                Debug.Log("Purple");
+
+                return true;
+            }
+        }
+        else if (mouseButton == 1)
+        {
+            m_mat.material.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+
+            return true;
+        }
+
+        return false;
     }
 }
