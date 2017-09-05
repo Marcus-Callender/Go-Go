8951af35e812d272615aff0e36e8d4d6ec36a896
Marcus Callender <Marcus-Callender@users.noreply.github.com>
2017-09-05 17:39:08 +0100

    Added more to the scene

---
+++ Go-Go/Assets/Code/Players.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Players : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Go-Go/Assets/Code/Stone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+    Material m_mat;
+
+    void Start()
+    {
+        m_mat = GetComponent<Material>();
+
+        m_mat.SetColor("clear", Color.clear);
+    }
+    
+    void Update()
+    {
+
+    }
+}
