cd70d364c7e68047c4637b7dc58af9bd15696caa
Marcus Callender <Marcus-Callender@users.noreply.github.com>
2017-09-05 19:17:30 +0100

    Made the stone able to change there colour

--- Go-Go/Assets/Code/Stone.cs
+++ Go-Go/Assets/Code/Stone.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Stone : MonoBehaviour
 {
-    Material m_mat;
+    Renderer m_mat;
 
     void Start()
     {
-        m_mat = GetComponent<Material>();
-
-        m_mat.SetColor("clear", Color.clear);
+        m_mat = GetComponent<Renderer>();
+        
+        m_mat.material.color = Color.yellow;
+        m_mat.material.color = new Color(1.0f, 0.92f, 0.016f, 1.0f);
     }
     
     void Update()
